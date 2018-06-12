5cd894336537b478242df1b6e9e64c6003303113
zamhaq <haque864@gmail.com>
2018-06-12 12:47:36 +0530

    Render boards through data array

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Hi"/>
+    <kanban msg="Hi"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Kanban from './components/Kanban.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Kanban
   }
 }
 </script>
---
+++ src/components/Kanban.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="kanban-container">
+  <div class="kanban-board" v-for="board in boards" :key="board.name">
+    {{board.name}}
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      boards: [{ name: "board1" }, { name: "board2" }, { name: "board3" }]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.kanban-container {
+  display: flex;
+  margin: 0 auto;
+  justify-content: space-around;
+}
+.kanban-board {
+  min-height: 300px;
+  width: 300px;
+  background-color: #cccccc;
+}
+</style>
